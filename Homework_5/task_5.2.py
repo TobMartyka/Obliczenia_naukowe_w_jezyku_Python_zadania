742d0addd99a6e13879a79486a22b4535b1adc5d
TobMartyka <162902255+TobMartyka@users.noreply.github.com>
2024-04-08 09:00:13 +0200

    Update task_5.2.py

--- Homework_5/task_5.2.py
+++ Homework_5/task_5.2.py
@@ -1,1 +1,13 @@
+from datetime import datetime, timedelta
 
+def print_working_days(date1, date2):
+    start_date = datetime.strptime(date1, '%Y-%m-%d')
+    end_date = datetime.strptime(date2, '%Y-%m-%d')
+
+    current_date = start_date
+    while current_date < end_date:
+        if current_date.weekday() < 5:
+            print(current_date.date())
+        current_date += timedelta(days=1)
+
+print_working_days("1999-09-18", "1999-10-18")
