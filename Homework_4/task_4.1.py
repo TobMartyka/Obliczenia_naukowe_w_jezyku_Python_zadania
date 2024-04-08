1efe11d3e2df190ecb1de74124bc9f2b885f97bb
TobMartyka <162902255+TobMartyka@users.noreply.github.com>
2024-04-08 08:57:20 +0200

    Update task_4.1.py

--- Homework_4/task_4.1.py
+++ Homework_4/task_4.1.py
@@ -1,1 +1,16 @@
+unit_circle = lambda p: p[0]**2 + p[1]**2 <= 1
 
+positiv_cord = lambda p: p[0] > 0 and p[1] > 0
+
+sorting_key_x_inc_y_dec = lambda p: (-p[1], p[0])
+
+sorting_key_sum = lambda p: abs(p[0]) + abs(p[1])
+
+L = [(1,2), (-3, 4), (0, 0.5), (2, -1)]
+print("unit", list(filter(unit_circle, L)))
+print("pos", list(filter(positiv_cord, L)))
+for p in L:
+    print(p, unit_circle(p), positiv_cord(p))
+
+L.sort(key=lambda p: (-p[1], p[0]))
+print(L, "y decreasing, x increasing")
