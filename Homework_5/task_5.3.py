92cbbb19f3d954d6bca80e2620b2ef6daf299e71
TobMartyka <162902255+TobMartyka@users.noreply.github.com>
2024-04-08 09:00:31 +0200

    Update task_5.3.py

--- Homework_5/task_5.3.py
+++ Homework_5/task_5.3.py
@@ -1,1 +1,16 @@
+import random
 
+def random_walk(start):
+    position = 0
+    for i in range(start):
+        step = random.choice([-1, 1])
+        position += step
+        yield position
+
+final = random_walk(100)
+
+final_position = None
+for position in final:
+    final_position = position
+
+print(final_position)
