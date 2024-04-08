4f50a492a9e26efd4bdba470dd970c0eec41e63e
TobMartyka <162902255+TobMartyka@users.noreply.github.com>
2024-04-08 08:58:11 +0200

    Update task_4.3.py

--- Homework_4/task_4.3.py
+++ Homework_4/task_4.3.py
@@ -1,1 +1,22 @@
+def iter_even():
+    x = 0
+    while True:
+        yield x
+        x += 2
 
+def iter_odd():
+    x = 1
+    while True:
+        yield x
+        x += 2
+
+def iter_power(k):
+    x = 1
+    while True:
+        yield x
+        x *= k
+
+print("|even|odd|power|")
+even, odd, power = iter_even(), iter_odd(), iter_power(2)
+for i in range(10):
+    print(f"|{next(even):4}|", f"{next(odd):2}|", f"{next(power):4}|")
