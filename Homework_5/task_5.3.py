9f282fe36997298721d5eec67f5b22cb165bda11
TobMartyka <162902255+TobMartyka@users.noreply.github.com>
2024-04-08 11:00:49 +0200

    Update task_5.3.py

--- Homework_5/task_5.3.py
+++ Homework_5/task_5.3.py
@@ -1,16 +1,15 @@
 import random
 
-def random_walk(start):
-    position = 0
-    for i in range(start):
+def random_walk(start=0):
+    position = start
+    while True:
         step = random.choice([-1, 1])
         position += step
         yield position
-
-final = random_walk(100)
-
-final_position = None
-for position in final:
-    final_position = position
-
-print(final_position)
+results = []
+for iteration in range(10):
+    walker = random_walk()
+    for step in range(100):
+        next(walker)
+    results.append(next(walker))
+print(results)
