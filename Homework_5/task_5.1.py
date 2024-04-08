d7c47383bc52224046a7458cfcd339c424353dfc
TobMartyka <162902255+TobMartyka@users.noreply.github.com>
2024-04-08 08:59:00 +0200

    Update task_5.1.py

--- Homework_5/task_5.1.py
+++ Homework_5/task_5.1.py
@@ -1,1 +1,11 @@
+import os
 
+def find_pdf_size(top):
+    total_size = 0
+    for root, dirs, files in os.walk(top):
+        for file in files:
+            if file.endswith('.pdf'):
+                total_size += os.path.getsize(os.path.join(root, file))
+    return total_size
+
+print(find_pdf_size("."))
